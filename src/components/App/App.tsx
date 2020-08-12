d815c97e1c96de5dc61dcac164ab01cb45ddbaac
Vezbe PIVT <primer.aplikacija@gmail.com>
2020-08-13 01:31:14 +0200

    Implementirati jednu komponentu - MainMenu

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,9 +4,11 @@ import { Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 
+
 function App() {
   return (
     <Container>
+      
       <FontAwesomeIcon icon={ faHome } />      
       Home
     </Container>
